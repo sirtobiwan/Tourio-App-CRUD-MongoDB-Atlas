c9e75f89082e36afb99130754a6c8d68579aff34
Tobias Schweizer <tobias-schweizer@gmx.de>
2023-04-20 15:29:17 +0200

    add read from database

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,18 @@
-import { places } from '../../../../lib/db.js';
+// api/jokes/[id].js
+import dbConnect from "../../../../db/connect";
+import Place from "../../../../db/models/Places";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
+console.log(id)
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
 
-  if (!id) {
-    return;
+    if (!place) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json(place);
   }
-
-  const place = places.find((place) => place.id === id);
-
-  if (!place) {
-    return response.status(404).json({ status: 'Not found' });
-  }
-
-  response.status(200).json(place);
 }
