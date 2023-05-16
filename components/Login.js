31eff5a0d010180f20bc1fd905a63cbd6167a0c5
Tobias Schweizer <tobias-schweizer@gmx.de>
2023-05-16 16:22:22 +0200

    styling

--- components/Login.js
+++ components/Login.js
@@ -1,20 +1,35 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #f1f1f1;
+  color: #333;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  margin: 10px 0;
+`;
 
 export default function Login() {
   const { data: session } = useSession();
   console.log(session);
   if (session) {
     return (
-      <>
+      <Container>
         Signed in as {session.user.name} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+        <Button onClick={() => signOut()}>Sign out</Button>
+      </Container>
     );
   }
   return (
-    <>
+    <Container>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+      <Button onClick={() => signIn()}>Sign in</Button>
+    </Container>
   );
 }
+
